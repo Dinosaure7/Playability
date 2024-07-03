32e47a4a4102aba77f48d56960a2ebab5bb86dbc
Enzo Michel <enzomichel399@gmail.com>
2024-07-03 17:21:13 +0200

    dsfbjzen

--- dev-react/src/pages/Game.jsx
+++ dev-react/src/pages/Game.jsx
@@ -1,8 +1,9 @@
-
 function Game() {
   return (
-    <div>Game</div>
-  )
+    <main className="flex flex-col">
+      <h1 className="self-center text-2xl mt-6 text-[]">Liste des jeux-vidéos</h1>
+    </main>
+  );
 }
 
-export default Game
+export default Game;
