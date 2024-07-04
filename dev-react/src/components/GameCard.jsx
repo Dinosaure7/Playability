8ecd2211c7663d250dc85091a0bc346f75233666
Enzo Michel <enzomichel399@gmail.com>
2024-07-04 21:44:18 +0200

    fejnf

--- dev-react/src/components/GameCard.jsx
+++ dev-react/src/components/GameCard.jsx
@@ -70,13 +70,8 @@ function GameCard(gameData = {gameData}) {
                   </div>
                 </div>
               </div>
-<<<<<<< HEAD
-              <Link to="/game/:id">
-                <button className="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 bg-indigo-700 hover:bg-indigo-600 font-bold text-white md:text-lg rounded-lg shadow-md">
-=======
               <Link to="/gameList/:id">
                 <button className="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 bg-[var(--primary-color)] hover:bg-[var(--primary-hover-color)] font-bold text-white md:text-lg rounded-lg shadow-md">
->>>>>>> 9c447550d5c78c4526f4cff549cb43388cd2b61d
                   Voir plus
                 </button>
               </Link>
