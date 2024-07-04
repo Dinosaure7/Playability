966fdd0a18ec0802aa6a8b36c94537f4f25123df
Enzo Michel <enzomichel399@gmail.com>
2024-07-04 20:06:08 +0200

    djsfn

--- dev-react/src/pages/Home.jsx
+++ dev-react/src/pages/Home.jsx
@@ -1,19 +1,28 @@
+import { Link } from "react-router-dom";
+
 import GameCard from "../components/GameCard";
 import PeriphCard from "../components/PeriphCard";
 
 function Home() {
   return (
     <>
-      <h2 className="md:text-2xl text-[var(--white-color)] font-medium m-5">
+      <h2 className="text-[var(--white-color)] text-xl md:text-4xl font-medium m-5 md:ml-16">
         Découvrez les notes de nos experts en accessibilité et inclusivité
       </h2>
-      <section className="flex flex-wrap justify-center">
+      <section className="flex flex-wrap justify-center mb-10">
         <GameCard />
         <GameCard />
         <GameCard />
         <GameCard />
       </section>
-      <h2 className="md:text-2xl text-[var(--white-color)] font-medium m-5">
+      <Link
+        to="/gameList"
+        className="text-[var(--white-color)] flex gap-2 mb-16 md:mb-28 justify-end underline md:text-xl mr-10 md:mr-14 hover:text-[var(--primary-color)]"
+      >
+        Parcourir la liste
+        <img className="w-5" src="/src/assets/arrowNext.svg" alt="" />
+      </Link>
+      <h2 className="md:ml-16 text-[var(--white-color)] text-xl md:text-4xl m-5">
         Découvrez les périphériques recommandés par nos experts en accessibilité
       </h2>
       <section className="flex flex-wrap justify-center">
@@ -22,6 +31,13 @@ function Home() {
         <PeriphCard />
         <PeriphCard />
       </section>
+      <Link
+        to="/gameList"
+        className="text-[var(--white-color)] flex gap-2 mb-16 md:mb-28 justify-end underline md:text-xl mr-10 md:mr-14 hover:text-[var(--primary-color)]"
+      >
+        Parcourir la liste
+        <img className="w-5" src="/src/assets/arrowNext.svg" alt="" />
+      </Link>
     </>
   );
 }
