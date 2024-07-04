4c771484d0202e9a7f278e945609c6fb663b1cce
Enzo Michel <enzomichel399@gmail.com>
2024-07-04 11:51:10 +0200

    jnsjdnfj

--- dev-react/src/App.jsx
+++ dev-react/src/App.jsx
@@ -1,7 +1,15 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <Outlet />;
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
---
+++ dev-react/src/components/CardComment.jsx
@@ -0,0 +1,42 @@
+function CardComment() {
+  return (
+    <section className="bg-[var(--nuance3-secondary)] flex flex-col gap-4 px-4 py-4 rounded-md">
+      <h3 className="text-[var(--primary-color)] opacity-80">
+        Félicien | Expert Testeur
+      </h3>
+      <p className="italic text-[var(--white-color)] font-light text-sm">
+        “Jeu vidéo très accessible et inclusif, peut convenir selon moi à tous
+        les publics”
+      </p>
+      <div className="flex gap-7">
+        <div className="flex gap-2 items-center">
+          <p className="text-xs text-[var(--white-color)] font-light">
+            Accessibilité
+          </p>
+          <p className="text-xs text-[var(--white-color)] flex gap-1 items-center">
+            7/10
+            <img className="w-3" src="/src/assets/Star.svg" alt="" />
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <p className="text-xs text-[var(--white-color)] font-light">
+            Inclusivité
+          </p>
+          <p className="text-xs text-[var(--white-color)] flex gap-1 items-center">
+            7/10
+            <img className="w-3" src="/src/assets/Star.svg" alt="" />
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-1">
+        <p className="text-xs text-[var(--white-color)] font-light">Globale</p>
+        <p className="text-xs text-[var(--white-color)] flex gap-1">
+          7/10
+          <img className="w-3" src="/src/assets/Star.svg" alt="" />
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default CardComment;
