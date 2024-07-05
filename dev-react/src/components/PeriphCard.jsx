b3fd0af8d269b3ed31b1a2165bce0746139ca942
FGAT3 <feliciengamelin@yahoo.fr>
2024-07-05 05:33:15 +0200

    rklbsveiuhisvqrbvsishivkzvvlkl

--- dev-react/src/components/PeriphCard.jsx
+++ dev-react/src/components/PeriphCard.jsx
@@ -1,20 +1,68 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import truncateText from "../service/delgorithm";
+// import imageController from `../assets/image-controller`;
+// import "../assets/image-controller";
+import img1 from "../assets/image-controller/xbox.jpg";
+import img2 from "../assets/image-controller/sony.jpg";
+import img3 from "../assets/image-controller/logi.png";
+import img4 from "../assets/image-controller/quadstick.png";
+import img5 from "../assets/image-controller/tobii.jpg";
+import img6 from "../assets/image-controller/able.jpg";
+import img7 from "../assets/image-controller/hori.jpg";
+import img8 from "../assets/image-controller/ben.jpg";
+
+// Fonction pour obtenir l'image par ID
+const getImageById = (id) => {
+  switch (id) {
+    case 1:
+      return img1;
+    case 2:
+      return img2;
+    case 3:
+      return img3;
+    case 4:
+      return img4;
+    case 5:
+      return img5;
+    case 6:
+      return img6;
+    case 7:
+      return img7;
+    case 8:
+      return img8;
+    default:
+      return null; // Optionnel: Ajouter une image par défaut ici si nécessaire
+  }
+};
+// const pathImages = "../assets/image-controller";
 
 function PeriphCard({ controller }) {
   const scrollToTop = window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
-  const { id, name, description, imageURL, imageAlt, youtubeID } = controller;
-  console.log(controller);
+
+  const { id, name, description, imageURL, src, imageAlt, youtubeID } =
+    controller;
+
+    // const imagePath = `${import.meta.env.VITE_API_URL}/assets/image-controller/${src}`;
+    // const imagePath = require(`../assets/image-controller/${src}`).default;
+    const imagePath = getImageById(id);
+
+  const isLongName = name.length > 30;
+
+  const maxDescriptionLength = isLongName ? 100 : 150;
+  const truncatedDescription = description
+    ? truncateText(description, maxDescriptionLength)
+    : "";
 
   return (
     <div className="max-w-md md:max-w-2xl px-5 my-5">
       <div className="bg-white shadow-xl rounded-lg overflow-hidden flex flex-col md:flex-row">
         <img
           className="w-full h-56 md:h-auto md:w-56 object-contain shadow-right"
-          src={imageURL}
+          src={imagePath}
           alt={imageAlt}
         />
         <div className="flex flex-col justify-between">
@@ -22,9 +70,7 @@ function PeriphCard({ controller }) {
             <p className="text-gray-800 font-bold text-xl md:text-2xl">
               {name}
             </p>
-            <p className="text-gray-700 md:text-lg">
-              {description}
-            </p>
+            <p className="text-gray-700 md:text-lg">{truncatedDescription}</p>
           </div>
           <div className="p-4 md:p-5 shadow-top bg-gray-100">
             <div className="sm:flex sm:justify-between sm:items-center">
